b6d1c32dbf7090e634caaa47073fb94bbbc30675
ritam14 <ritamsingha1992@gmail.com>
2020-11-02 17:45:51 +0530

    Factorial

--- GitPrac/src/Test/One.java
+++ GitPrac/src/Test/One.java
@@ -5,6 +5,13 @@ public class One {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
+		int no = 5, fact = 1;
+		boolean flag = false;
+		for(int i =2; i<=no; ++i)
+		{
+			fact = fact*i;
+		}
+		System.out.println(fact);
 	}
 
 }
